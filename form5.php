9838a6ee79a52a342d528d8c72d9fabc86813bc9
sarah <sarah.ahmed@freedomdev.com>
2021-03-20 12:12:58 +0300

    form5: sanitise fields

--- form5.php
+++ form5.php
@@ -1,16 +1,19 @@
 <h2>Form 1 - POST request with Sanitised Fields</h2>
 <?php
-$oldinput = $_GET['favouriteColor'];
+if (!empty($_GET)) {
+    $oldinput = $_GET['favouriteColor'];
+}
 ?>
 <form action="form5.php" method="POST">
     <label>What is your favourite color?
-        <input type="text" name="favouriteColor" value="<?php echo htmlspecialchars($oldinput); ?>" />
+        <input type="text" name="favouriteColor" value="<?php htmlentities($oldinput); ?>" />
     </label>
     <input type="submit" value='Send'>
 </form>
 
 <?php
 if (!empty($_GET)) {
+    $oldinput = $_GET['favouriteColor'];
     echo '<pre>';
     var_dump($_GET);
     echo '</pre>';
